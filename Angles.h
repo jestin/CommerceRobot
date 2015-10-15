e60efc865e766efc16bc66001cc4cf4de8a101e8
jestin <jestin.stoffel@commercebank.com>
2015-10-15 16:24:47 -0500

    Adding a separate file for individual calibrations

---
+++ Angles.h
@@ -0,0 +1,56 @@
+#ifndef ANGLES_H
+#define ANGLES_H
+
+#define LEG1_X_0 0
+#define LEG1_X_1 0
+#define LEG1_X_2 0
+#define LEG1_X_3 0
+#define LEG1_X_4 0
+
+#define LEG1_Y_0 0
+#define LEG1_Y_1 0
+#define LEG1_Y_2 0
+
+#define LEG2_X_0 0
+#define LEG2_X_1 0
+#define LEG2_X_2 0
+#define LEG2_X_3 0
+#define LEG2_X_4 0
+
+#define LEG2_Y_0 0
+#define LEG2_Y_1 0
+#define LEG2_Y_2 0
+
+#define LEG3_X_0 0
+#define LEG3_X_1 0
+#define LEG3_X_2 0
+#define LEG3_X_3 0
+#define LEG3_X_4 0
+
+#define LEG3_Y_0 0
+#define LEG3_Y_1 0
+#define LEG3_Y_2 0
+
+#define LEG4_X_0 0
+#define LEG4_X_1 0
+#define LEG4_X_2 0
+#define LEG4_X_3 0
+#define LEG4_X_4 0
+
+#define LEG4_Y_0 0
+#define LEG4_Y_1 0
+#define LEG4_Y_2 0
+
+#define LEG1_X_ANGLES {LEG1_X_0, LEG1_X_1, LEG1_X_2, LEG1_X_3, LEG1_X_4}
+#define LEG1_Y_ANGLES {LEG1_Y_0, LEG1_Y_1, LEG1_Y_2}
+
+#define LEG2_X_ANGLES {LEG2_X_0, LEG2_X_1, LEG2_X_2, LEG2_X_3, LEG2_X_4}
+#define LEG2_Y_ANGLES {LEG2_Y_0, LEG2_Y_1, LEG2_Y_2}
+
+#define LEG3_X_ANGLES {LEG3_X_0, LEG3_X_1, LEG3_X_2, LEG3_X_3, LEG3_X_4}
+#define LEG3_Y_ANGLES {LEG3_Y_0, LEG3_Y_1, LEG3_Y_2}
+
+#define LEG4_X_ANGLES {LEG4_X_0, LEG4_X_1, LEG4_X_2, LEG4_X_3, LEG4_X_4}
+#define LEG4_Y_ANGLES {LEG4_Y_0, LEG4_Y_1, LEG4_Y_2}
+
+#endif
