da1fdf6a82f2a064da7d4193f3c74d81766ed113
Jestin Stoffel <jestin.stoffel@gmail.com>
2015-11-20 23:03:41 -0600

    calibrated the robot and set a simple routine for it to do

--- Angles.h
+++ Angles.h
@@ -1,45 +1,45 @@
 #ifndef ANGLES_H
 #define ANGLES_H
 
-#define LEG1_X_0 0
-#define LEG1_X_1 0
-#define LEG1_X_2 0
-#define LEG1_X_3 0
-#define LEG1_X_4 0
+#define LEG1_X_0 15
+#define LEG1_X_1 58
+#define LEG1_X_2 102
+#define LEG1_X_3 141
+#define LEG1_X_4 180
 
-#define LEG1_Y_0 0
-#define LEG1_Y_1 0
-#define LEG1_Y_2 0
+#define LEG1_Y_0 70
+#define LEG1_Y_1 115
+#define LEG1_Y_2 160
 
-#define LEG2_X_0 0
-#define LEG2_X_1 0
-#define LEG2_X_2 0
-#define LEG2_X_3 0
-#define LEG2_X_4 0
+#define LEG2_X_0 18
+#define LEG2_X_1 58
+#define LEG2_X_2 99
+#define LEG2_X_3 139
+#define LEG2_X_4 180
 
-#define LEG2_Y_0 0
-#define LEG2_Y_1 0
-#define LEG2_Y_2 0
+#define LEG2_Y_0 105
+#define LEG2_Y_1 145
+#define LEG2_Y_2 180
 
-#define LEG3_X_0 0
-#define LEG3_X_1 0
-#define LEG3_X_2 0
-#define LEG3_X_3 0
-#define LEG3_X_4 0
+#define LEG3_X_0 15
+#define LEG3_X_1 58
+#define LEG3_X_2 102
+#define LEG3_X_3 141
+#define LEG3_X_4 180
 
-#define LEG3_Y_0 0
-#define LEG3_Y_1 0
-#define LEG3_Y_2 0
+#define LEG3_Y_0 105
+#define LEG3_Y_1 145
+#define LEG3_Y_2 180
 
-#define LEG4_X_0 0
-#define LEG4_X_1 0
-#define LEG4_X_2 0
-#define LEG4_X_3 0
-#define LEG4_X_4 0
+#define LEG4_X_0 10
+#define LEG4_X_1 53
+#define LEG4_X_2 95
+#define LEG4_X_3 135
+#define LEG4_X_4 175
 
-#define LEG4_Y_0 0
-#define LEG4_Y_1 0
-#define LEG4_Y_2 0
+#define LEG4_Y_0 60
+#define LEG4_Y_1 110
+#define LEG4_Y_2 150
 
 #define LEG1_X_ANGLES {LEG1_X_0, LEG1_X_1, LEG1_X_2, LEG1_X_3, LEG1_X_4}
 #define LEG1_Y_ANGLES {LEG1_Y_0, LEG1_Y_1, LEG1_Y_2}
--- CommerceRobot.ino
+++ CommerceRobot.ino
@@ -4,6 +4,7 @@
 // Leg 4: 10,11
 
 #include "Leg.h"
+#include "Angles.h"
 #include <Servo.h>
 
 Leg leg1;
@@ -12,6 +13,22 @@ Leg leg3;
 Leg leg4;
 
 void setup(){
+  int leg1XAngles[5] = LEG1_X_ANGLES;
+  int leg1YAngles[3] = LEG1_Y_ANGLES;
+  leg1.setAngles(leg1XAngles, 5, leg1YAngles, 3);
+
+  int leg2XAngles[5] = LEG2_X_ANGLES;
+  int leg2YAngles[3] = LEG2_Y_ANGLES;
+  leg2.setAngles(leg2XAngles, 5, leg2YAngles, 3);
+
+  int leg3XAngles[5] = LEG3_X_ANGLES;
+  int leg3YAngles[3] = LEG3_Y_ANGLES;
+  leg3.setAngles(leg3XAngles, 5, leg3YAngles, 3);
+
+  int leg4XAngles[5] = LEG4_X_ANGLES;
+  int leg4YAngles[3] = LEG4_Y_ANGLES;
+  leg4.setAngles(leg4XAngles, 5, leg4YAngles, 3);
+  
   leg1.attach(3, 5);
   leg2.attach(6, 7);
   leg3.attach(8, 9);
@@ -19,4 +36,10 @@ void setup(){
 }
 
 void loop(){
+  for(int x = 0; x < 5; x ++){
+    for(int y = 0; y < 3; y++){
+      leg1.setPosition(x, y);
+      delay(500);
+    }
+  }
 }
